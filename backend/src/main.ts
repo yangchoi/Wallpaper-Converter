ef0e85b4a97f9551ce031afdd484c9b446d871bb
yangchoi <yangchoi.hj@gmail.com>
2025-08-12 00:39:56 +0900

    fix(backend): fix fort

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,16 +5,9 @@ import { ensureDir } from 'fs-extra';
 import { join } from 'path';
 
 async function bootstrap() {
-  await ensureDir(join(process.cwd(), 'uploads'));
-  await ensureDir(join(process.cwd(), 'converted'));
-
   const app = await NestFactory.create(AppModule, { cors: true });
-
-  app.use(json({ limit: '200mb' }));
-  app.use(urlencoded({ limit: '200mb', extended: true }));
-
-  const port = process.env.PORT || 4000;
-  await app.listen(port);
+  const port = Number(process.env.PORT) || 8000;
+  await app.listen(port, '0.0.0.0');
   console.log(`Nest API running on http://localhost:${port}`);
 }
 bootstrap();
